356a82da4b30d832bc369186682146ef4a3428b9
Jyoti Ola <macbookair@Jyotis-MacBook-Air.local>
2026-05-28 21:48:22 +0530

    created todo app

--- README.md
+++ README.md
@@ -19,5 +19,8 @@ bun start
 ```
 
 This project was created using `bun init` in bun v1.3.12. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
+<<<<<<< HEAD
 
 here i have wrote a wrapper component named CardWrapper which helps in rendering the same type of components and it make lyf easier in fe development . Card wrapper accepts children as it props(only children ) 
+=======
+>>>>>>> 69fa0e6 (created todo app)
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import "./index.css";
 export function App() {
@@ -33,3 +34,55 @@ function CardWrapper(props : ICard){
 }
 
 export default App;
+=======
+import { useEffect, useState } from "react";
+import "./index.css";
+import axios from "axios";
+import { response } from "express";
+
+
+export function App(){
+  return(
+    <div>
+     <Todo/>
+    </div>
+  )
+}
+interface ITodo{
+  title : string,
+  completed : boolean
+}
+
+function Todo(){
+  const[todo , setTodo] = useState<ITodo>();
+  const[selectedId , setSelectedId] = useState(0);
+
+ useEffect(()=>{
+  axios.get("https://jsonplaceholder.typicode.com/todos?id=" + selectedId)
+  .then(response=>{
+    setTodo(response.data[0])
+  })
+ },[selectedId])
+  return(
+    <div>
+    <div style={{display : "flex", gap : 20}}>
+      <button onClick={function main(){
+        setSelectedId(1)
+      }}>1</button>
+
+    <button onClick={function(){
+        setSelectedId(2)
+   }}>2</button>
+  
+    <button onClick={function(){
+        setSelectedId(3)
+  }}>3</button>
+     
+
+ </div>
+     <h1>{todo?.title}</h1>
+    </div>
+  )
+}
+export default App
+>>>>>>> 69fa0e6 (created todo app)
